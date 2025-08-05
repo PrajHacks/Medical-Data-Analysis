a68beee3a45ea6969f7201aa7216c20fdf8e2f25
Prajwal-hecker <prajwalshevante1@gmail.com>
2025-08-05 16:49:11 +0530

    Added regression analysis and AUC support to Statease

--- medical_analysis_app.py
+++ medical_analysis_app.py
@@ -16,7 +16,7 @@ st.set_page_config(layout="wide", page_title="Medical Data Analysis Hub 🩺")
 
 # --- Home Page ---
 def home_page():
-    st.title("Medical Data Analysis Hub")
+    st.title("StatEase - Medical Data Analysis Hub")
     st.markdown("""
     Welcome to your user-friendly statistical analysis software designed for medical professionals.
     This tool helps you analyze clinical data with ease, even with minimal statistical and coding knowledge.
